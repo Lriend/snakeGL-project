6050aa6f97b13b4fad6756456280ed51b7d10e3c
Marek Piętka <marek.pietka@onet.eu>
2019-04-11 01:21:56 +0200

    libraries.h created

--- snakeGL/main.cpp
+++ snakeGL/main.cpp
@@ -1,20 +1,4 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <vector>
-
-#include "GL/glew.h"
-#include "GLFW/glfw3.h"
-
-#include "glm/glm.hpp"
-#include "glm/vec2.hpp"
-#include "glm/vec3.hpp"
-#include "glm/vec4.hpp"
-#include "glm/mat4x4.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtc/type_ptr.hpp"
-
-#include <SOIL2.h>
+#include "libraries.h"
 
 struct Vertex {
 	glm::vec3 position;
