6bbdcde6c9c49464c45e02aec2d5e3b91d88628d
Marek Piętka <marek.pietka@onet.eu>
2019-05-24 07:48:42 +0200

    We don't want "Lorem Ipsum Title", do we?

--- snakeGL/main.cpp
+++ snakeGL/main.cpp
@@ -1,7 +1,7 @@
 #include "game.h"
 
 int main() {
-		Game snake("Lorem Ipsum Title", 1920, 1080, 4, 4, true);
+		Game snake("SnakeGL by Lriend", 1920, 1080, 4, 4, true);
 	//MAIN LOOP
 	while (!snake.getWindowShouldClose()) {
 		snake.update();
